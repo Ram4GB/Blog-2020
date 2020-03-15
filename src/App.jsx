7b783d844c1ec262cfb17cc4ebe61118497b5ef0
Ram4GB <leminhcuong2988@yahoo.com.vn>
2020-03-15 10:53:26 +0700

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code>
         </p>
+        Edit and save to reload.
         <a
           className="App-link"
           href="https://reactjs.org"
