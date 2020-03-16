c9251ecff4a28d16e0a02c9619d0f8cabe6c4f09
Ram4GB <leminhcuong2988@yahoo.com.vn>
2020-03-16 21:59:34 +0700

    build app react

---
+++ docs/precache-manifest.a05d014a7639de0a93a0bc5f68986814.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fabf4d05a50c55e3737da10e0f3e643c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f640cff1fe0356de89b2",
+    "url": "/static/css/2.16851201.chunk.css"
+  },
+  {
+    "revision": "275a5021a5fdb40a99cd",
+    "url": "/static/css/main.49e87a8a.chunk.css"
+  },
+  {
+    "revision": "f640cff1fe0356de89b2",
+    "url": "/static/js/2.d972409c.chunk.js"
+  },
+  {
+    "revision": "9cacdb0c4185ab3e358d4e757e54de21",
+    "url": "/static/js/2.d972409c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "275a5021a5fdb40a99cd",
+    "url": "/static/js/main.82623107.chunk.js"
+  },
+  {
+    "revision": "7df05647dafffe682ceb",
+    "url": "/static/js/runtime-main.d92a30dd.js"
+  }
+]);
